4873d523dfd38fd7c6552fbf4d4ef0ca8aa701b3
m1st1nh0 <lukaschamposki@gmail.com>
2025-10-25 16:28:30 -0300

    Segunda tentativa do desafio

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 let nomeHeroi = "Jonas Heróico";
-let Xp = "321841";
+let Xp = 321841;
 let nivelHeroi = "";
-
-if (Xp < 1000){
-    nivelHeroi = "Ferro";
-} else if(Xp >= 1001){
-    nivelHeroi = "Bronze";
-} else if(Xp >= 2001){
-    nivelHeroi = "Prata";
-} else if(Xp >= 5001){
-    nivelHeroi = "Ouro";
-} else if(Xp >= 7001){
-    nivelHeroi = "Platina";
-} else if(Xp >= 8001){
-    nivelHeroi = "Ascendente";
+if(Xp >= 10001){
+    nivelHeroi = "Radiante";
 }else if(Xp >= 9001){
     nivelHeroi = "Imortal";
+}else if(Xp >= 8001){
+    nivelHeroi = "Ascendente";
+}else if(Xp >= 7001){
+    nivelHeroi = "Platina";
+}else if(Xp >= 5001){
+    nivelHeroi = "Ouro";
+}else if(Xp >= 2001){
+    nivelHeroi = "Prata";
 }else if(Xp >= 1001){
-    nivelHeroi = "Radiante";
-}
+    nivelHeroi = "Bronze";
+}else if (Xp < 1000){
+    nivelHeroi = "Ferro";
+}     
+
 
 console.log("O Herói de nome "+ nomeHeroi+" Esta no nível " +nivelHeroi)
