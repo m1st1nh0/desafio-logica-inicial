27510affadcb4d74d23e2f7685be34d4fa4e7bf8
m1st1nh0 <lukaschamposki@gmail.com>
2025-10-25 16:16:45 -0300

    Primeira tentativa do desafio

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("fala mundoquet")
+let nomeHeroi = "Jonas Heróico";
+let Xp = "321841";
+let nivelHeroi = "";
+
+if (Xp < 1000){
+    nivelHeroi = "Ferro";
+} else if(Xp >= 1001){
+    nivelHeroi = "Bronze";
+} else if(Xp >= 2001){
+    nivelHeroi = "Prata";
+} else if(Xp >= 5001){
+    nivelHeroi = "Ouro";
+} else if(Xp >= 7001){
+    nivelHeroi = "Platina";
+} else if(Xp >= 8001){
+    nivelHeroi = "Ascendente";
+}else if(Xp >= 9001){
+    nivelHeroi = "Imortal";
+}else if(Xp >= 1001){
+    nivelHeroi = "Radiante";
+}
+
+console.log("O Herói de nome "+ nomeHeroi+" Esta no nível " +nivelHeroi)
